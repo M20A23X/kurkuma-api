85020040888be0e33e36d19e56854c973fb9ac46
Maksym Shapovalov <maks0681912507@gmail.com>
2024-01-08 21:35:40 -0500

    Initialize monorepo

---
+++ scripts/check-files.sh
@@ -0,0 +1,32 @@
+TEMP_FILE="CHECK_TEMP"
+PACKAGES_ROOT="packages/"
+DATABASE="database"
+
+package=$1
+
+if [[ "$package" == "$DATABASE" ]]; then
+  PACKAGES_DIR=''
+fi
+
+if [[ "$EVENT_NAME" == 'pull_request' ]]; then
+  diffRes=$(git diff --name-only -r HEAD^1 HEAD | xargs)
+else
+  if [[ "$GITHUB_REF" == "$BRANCH_DEV" ]]; then
+    diffRes=$(git diff --name-only $EVENT_BEFORE $EVENT_AFTER | xargs)
+  else
+    diffRes=$(git diff --name-only origin/dev $EVENT_AFTER | xargs)
+  fi
+fi
+
+echo "$diffRes" >$TEMP_FILE
+
+while IFS= read -r file; do
+  checkRes=$(echo "$file" | grep "$PACKAGES_ROOT$package/")
+  if [[ $checkRes ]]; then
+    echo "Changed files found since $BRANCH_DEV:"
+    echo "$checkRes"
+    echo "::set-output name=delta::true"
+    break
+  fi
+done <$TEMP_FILE
+rm $TEMP_FILE
