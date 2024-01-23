cb919fd60b30085bf955ac9765984a994ddc3a20
Maksym Shapovalov <maks0681912507@gmail.com>
2024-01-23 18:14:48 -0500

    Feat(DB): initialize database

---
+++ database/global.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE `kurkuma`;
+USE `kurkuma`;
+
+ALTER DATABASE `kurkuma` CHARACTER SET `utf8mb3` COLLATE `utf8mb3_general_ci`;
+SELECT @@`character_set_database`, @@`collation_database`;
